371d939bac53c5fd9ca18ff01876b61fff421bd6
amirsuhel88 <60836385+amirsuhel88@users.noreply.github.com>
2024-08-14 22:15:13 +0530

    router and controller for authenticatioin is added with json file having pre filled.

---
+++ controllers/authController.js
@@ -0,0 +1,35 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const users = require("../users.json");
+
+//authentication for sighn up.
+exports.signup = async (req, res) => {
+  const { username, password, email } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = {
+    id: uuidv4(),
+    username,
+    email,
+    password: hashedPassword,
+  };
+  users.push(newUser);
+  fs.writeFileSync("users.json", JSON.stringify(users));
+  res.satatus(201).json({
+    message: "User is successfully registered",
+  });
+};
+
+//authenticatoin for login
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((u) => u.username === username);
+  if (!user) return res.status(400).json({ message: "invalid credentials" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: "invalid credentials" });
+
+  const token = jwt.sign({ id: user.id }, "secret", { expiresIn: "1h" });
+  res.json({ token });
+};
