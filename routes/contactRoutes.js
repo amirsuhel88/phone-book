3e8b2e435e99017c8052bdf5e831b20d0ef5fa81
amirsuhel88 <60836385+amirsuhel88@users.noreply.github.com>
2024-08-15 22:00:45 +0530

    validation is working

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { createContact } = require("../controllers/contactController");
+const {
+  createContact,
+  updateContact,
+} = require("../controllers/contactController");
 const upload = require("../middleware/uploadMiddleware");
 
 router.post("/", upload.single("photo"), createContact);
+router.put("/:id", upload.single("photo"), updateContact);
 
 module.exports = router;
